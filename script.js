1014d65fc74dedd15c189f3f79d61e45d5f5570c
Troyano97 <ivanferreyradaniel@gmail.com>
2023-08-31 00:42:15 -0300

    arreglo explicacion

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const matriz = [];
 
 let numInicial = 1;
-//para mostrar 50 numeros cambiamos el valor maximo de columnas(i o j) a 5, 156 
+//para mostrar 50 numeros cambiamos el valor de la columna o fila (i o j) a 5,15,16,20 y el maximo a numInicial en 50 150,151,200
 for (let i = 0; i < 10; i++) {
     matriz.push([]);
     for (let j = 0; j < 10; j++) {
